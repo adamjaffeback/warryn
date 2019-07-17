3c74694d695f6193d74d96d6763569f14fbdfffe
Adam Jaffe Back <adb33@email.arizona.edu>
2019-07-17 12:14:05 -0700

    (refactor) Use array index to track order in issuesReducer.

--- src/state/reducers/issuesReducer.js
+++ src/state/reducers/issuesReducer.js
@@ -1,20 +1,9 @@
 export default (state = [], action) => {
   switch (action.type) {
     case 'ADD_ISSUES':
-      const orderIndexStart = state.length ? state.length : 0;
-      const newIssues = action.payload.reduce((acc, issue) => {
-        let [index, wipIssueList] = acc;
-        wipIssueList.push({
-          ...issue,
-          order: index,
-        });
-        index++;
-        return acc;
-      }, [orderIndexStart, []])[1];
-
       return [
         ...state,
-        ...newIssues,
+        ...action.payload,
       ];
   default:
    return state;
